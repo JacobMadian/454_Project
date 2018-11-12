4fe1a751193f32139081489bc194d7d1446d90c4
Jacob Madian <44143326+JacobMadian@users.noreply.github.com>
2018-11-11 20:19:54 -0800

    Added description to master file, and created Data Input file to serve as independent code dev of excel reading functions

---
+++ 454_Project.py
@@ -0,0 +1,4 @@
+"""
+The purpose of this file is to be the master file only including code proven to work.
+Once a section of code is proven to work independently it should be added in this file.
+"""
---
+++ Data Input.py
@@ -0,0 +1,3 @@
+"""
+This file is reserved for testing of the data input functions and code.
+"""
